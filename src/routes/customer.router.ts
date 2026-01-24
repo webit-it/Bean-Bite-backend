68179d809646ee7126ac5955052da5acd4da4d39
Mubariz Ibrahim <mubarismubuk.v@gmail.com>
2026-01-25 02:27:58 +0530

    feat: implement get and update user profile

--- src/routes/customer.router.ts
+++ src/routes/customer.router.ts
@@ -1,10 +1,13 @@
 import express from 'express'
 import { CustomerAuthRepository } from '../repositories/customer.auth.repository'
 import { CustomerAuthService } from '../services/customer/auth.customer.service'
-import { CustomerAuthController } from '../controllers/customer/authController'
 import { ProductService } from '../services/customer/product.service'
 import { ProductRepository } from '../repositories/product.repositry'
 import { ProductController } from '../controllers/customer/product.controller'
+import { ProfileService } from '../services/customer/profile.service'
+import { ProfileController } from '../controllers/customer/profile.controller'
+import { CustomerAuthController } from '../controllers/customer/authController'
+import { verifyToken } from '../middleware/auth.middleware'
 const router=express.Router()
 
 const customerAuthRepo=new CustomerAuthRepository()
@@ -15,12 +18,19 @@ const productRepo=new ProductRepository()
 const productService=new ProductService(productRepo)
 const productController=new ProductController(productService)
 
+const profileService=new ProfileService(customerAuthRepo)
+const profileController=new ProfileController(profileService)
+
+
 router.route("/register").post(authController.register);
 router.route("/otp/verify").post(authController.verifyOtp);
 router.route("/otp/resend").post(authController.resendOtp);
 router.route("/login").post(authController.login);
 router.get("/product/:slug", productController.getProductDetails);
 router.get("/product", productController.getProducts);
-
+router
+  .route("/profile")
+  .get(verifyToken, profileController.getProfile)
+  .put(verifyToken, profileController.editProfile);
 
 export default router 

4f3de6dfa4d308fc9912fcca627920fc01d38b8e
Mubariz Ibrahim <mubarismubuk.v@gmail.com>
2026-02-02 21:46:43 +0530

    feat(reward): update reward by level with product IDs and proper response

--- src/repositories/reward.repository.ts
+++ src/repositories/reward.repository.ts
@@ -1,6 +1,7 @@
+import { Types } from "mongoose";
 import { IRewardRepository } from "../interfaces/repository/reward.repository.interface";
 import { Reward } from "../models/reward.model";
-import { IRewardDocument } from "../types/reward.type";
+import { IReward, IRewardDocument } from "../types/reward.type";
 import { BaseRepository } from "./base.reposiory";
 
 export class RewardRepository extends BaseRepository<IRewardDocument> implements IRewardRepository {
@@ -14,12 +15,12 @@ export class RewardRepository extends BaseRepository<IRewardDocument> implements
                 select: "productName image",
             }).exec();
     }
-    async updateSlotCount(level: number, data: Partial<{ rewardName: string; slotCount: number }>) {
+    async updateRewardByLevel(level: number, data: Partial<IReward>) {
         return Reward.findOneAndUpdate(
             { level },
             { $set: data },
             { new: true }
-        );
+        )
     }
     async findAll() {
         return Reward.find().populate("rewardProducts", "productName image");

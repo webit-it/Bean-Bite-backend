cfa872b0262c70679df491767c9fbc56fa79f0f2
Mubariz Ibrahim <mubarismubuk.v@gmail.com>
2026-02-06 23:22:57 +0530

    feat: New methods added in reward progress repostory

--- src/interfaces/repository/reward.progress.repository.interface.ts
+++ src/interfaces/repository/reward.progress.repository.interface.ts
@@ -1,7 +1,22 @@
+import mongoose, { ClientSession } from "mongoose"
 import { ICustomerRewardProgress, ICustomerRewardProgressDocument } from "../../types/customerRewardProgress.type"
 
 export interface ICustomerRewardProgressRepository {
     create(data: Partial<ICustomerRewardProgress>): Promise<ICustomerRewardProgressDocument>
-    getLatestActiveProgress(customerId: string): Promise<ICustomerRewardProgressDocument | null>
-    incrementProgress(customerId: string,rewardId:string): Promise<ICustomerRewardProgressDocument|null>
+    getLatestActiveProgress(
+        customerId: mongoose.Types.ObjectId,
+        session?: ClientSession
+    ): Promise<ICustomerRewardProgressDocument | null>
+    incrementProgress(
+        progressId: mongoose.Types.ObjectId,
+        session?: ClientSession
+    ): Promise<ICustomerRewardProgressDocument | null>
+    markAsCompleted(
+        progressId: mongoose.Types.ObjectId,
+        session?: ClientSession
+    ): Promise<ICustomerRewardProgressDocument | null>
+    getCompletedProgress(
+        customerId: mongoose.Types.ObjectId,
+        session?: ClientSession
+    ): Promise<ICustomerRewardProgressDocument[]>
 }

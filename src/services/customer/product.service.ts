47af011c56080ea0f13e6ece0e08750ca08ff246
Mubariz Ibrahim <mubarismubuk.v@gmail.com>
2026-02-07 19:39:17 +0530

    feat: Dto added in product in customer side

--- src/services/customer/product.service.ts
+++ src/services/customer/product.service.ts
@@ -2,46 +2,47 @@ import HttpStatus from "../../constants/httpsStatusCode";
 import { Messages } from "../../constants/messages";
 import IProductRepository from "../../interfaces/repository/product.repository.interface";
 import { IProductService } from "../../interfaces/service/customer/customer.product.interface";
+import { ProductMapper } from "../../mappers/product.mapper";
 import AppError from "../../utils/AppError";
 
-export class ProductService implements IProductService{
-  constructor(private _productRepository:IProductRepository){}
+export class ProductService implements IProductService {
+  constructor(private _productRepository: IProductRepository) { }
   getProducts = async (
-  page: number,
-  limit: number,
-  search?: string,
-  category?: string
-) => {
-  const result = await this._productRepository.findAllPaginated(
-    page,
-    limit,
-    search,
-    category
-  );
+    page: number,
+    limit: number,
+    search?: string,
+    category?: string
+  ) => {
+    const result = await this._productRepository.findAllPaginated(
+      page,
+      limit,
+      search,
+      category
+    );
 
-  return {
-    data: result.data,
-    total: result.total,
-    page: result.page,
-    limit: result.limit,
-    totalPages: Math.ceil(result.total / result.limit),
+    return {
+      data: result.data.map(ProductMapper.toResponse),
+      total: result.total,
+      page: result.page,
+      limit: result.limit,
+      totalPages: Math.ceil(result.total / result.limit),
+    };
   };
-};
 
-    getProductDetails = async (slug: string) => {
-        try {
-          const product = await this._productRepository.findBySlug(slug);
-    
-          if (!product) {
-            throw new AppError(
-              Messages.PRODUCT_NOT_FOUND,
-              HttpStatus.NOT_FOUND
-            );
-          }
-          return product;
-        } catch (error) {
-          console.log("Get category for edit error:", error);
-          throw error;
-        }
-      };
+  getProductDetails = async (slug: string) => {
+    try {
+      const product = await this._productRepository.findBySlug(slug);
+
+      if (!product) {
+        throw new AppError(
+          Messages.PRODUCT_NOT_FOUND,
+          HttpStatus.NOT_FOUND
+        );
+      }
+      return ProductMapper.toResponse(product);
+    } catch (error) {
+      console.log("Get category for edit error:", error);
+      throw error;
+    }
+  };
 }

7cc42e9972cac10951fe0b4c05d61f470de5f239
Mubariz Ibrahim <mubarismubuk.v@gmail.com>
2026-02-20 23:02:07 +0530

    feat: findAll repo method added in reward history repository

--- src/types/reward.history.type.ts
+++ src/types/reward.history.type.ts
@@ -1,17 +1,58 @@
 import { Types, HydratedDocument } from "mongoose";
+import {  ICustomerDocument } from "./customer.type";
+import {  IRewardDocument } from "./reward.type";
+import {  IProductDocument } from "./product.type";
 
 export interface IRewardHistory {
-  customer: Types.ObjectId;
-  reward: Types.ObjectId;
+  _id:Types.ObjectId
+  customer: Types.ObjectId|ICustomerDocument;
+  reward: Types.ObjectId|IRewardDocument;
   level: number;
   slotCount: number;
-  status: "COMPLETED" | "REDEEMED" | "EXPIRED";
-  redeemedProduct?: Types.ObjectId;
-  redeemedByAdmin?: Types.ObjectId;
-  completedAt?: Date;
+  status: "COMPLETED" | "REDEEMED"|"IN_PROGRESS";
+  action: "SLOT_FILLED"| "LEVEL_COMPLETED";
+  redeemedProduct?: Types.ObjectId|null|IProductDocument;
+  completedAt?: Date|null;
   redeemedAt?: Date
   createdAt: Date;
   updatedAt: Date;
 }
 
 export type IRewardHistoryDocument = HydratedDocument<IRewardHistory>;
+
+
+
+export interface RewardHistoryResponseDto {
+  id: string;
+
+  level: number;
+  slotCount: number;
+
+  action: "SLOT_FILLED" | "LEVEL_COMPLETED";
+  status: "IN_PROGRESS" | "COMPLETED" | "REDEEMED";
+
+  completedAt?: Date;
+  createdAt: Date;
+  redeemedAt?: Date;
+  updatedAt: Date;
+
+  customer: {
+    id: string;
+    fullName: string;
+    phoneNumber: string;
+  };
+
+  reward: {
+    id: string;
+    rewardName: string;
+    slug: string;
+    slotCount: number;
+  };
+
+  redeemedProduct: {
+    id: string;
+    productName: string;
+    slug: string;
+    image?: string;
+  } | null;
+}
